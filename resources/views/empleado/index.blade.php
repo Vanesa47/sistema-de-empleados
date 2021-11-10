5d767b7a6cc2a4e56b7ea547cdc7d08d9081b389
Vanesa Cardona <vanesacardona076@gmail.com>
2021-11-10 15:43:56 -0600

    Mostrar la fotografía de Storage, creando enlace hacia mi carpeta

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -17,7 +17,12 @@ mostrar la lista de empleados
         @foreach($empleados as $empleado)
         <tr>
             <td>{{$empleado->id}}</td>
-            <td>{{$empleado->Foto}}</td>
+
+            <td>
+            <img src="{{asset('storage').'/'.$empleado->Foto}}" alt=""> 
+            
+            </td>
+
             <td>{{$empleado->Nombre}}</td>
             <td>{{$empleado->PrimerApellido}}</td>
             <td>{{$empleado->SegundoApellido}}</td>
